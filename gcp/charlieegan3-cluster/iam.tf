0a842f476a7a5ba103b2c669dbe7d5ecfd28a3c1
Charlie Egan <charlieegan3@users.noreply.github.com>
2019-12-14 16:15:12 +0000

    Add thanos iam config

--- gcp/charlieegan3-cluster/iam.tf
+++ gcp/charlieegan3-cluster/iam.tf
@@ -50,3 +50,31 @@ resource "google_service_account_iam_binding" "bigquery-user" {
 
   members = ["serviceAccount:charlieegan3-cluster.svc.id.goog[monitoring/gf-grafana]"]
 }
+
+resource "google_storage_bucket" "thanos-storage" {
+  name     = "charlieegan3-cluster-thanos"
+  location = "US"
+  project  = var.project_id
+}
+
+resource "google_storage_bucket_iam_binding" "thanos-binding" {
+  bucket = "${google_storage_bucket.thanos-storage.name}"
+  role   = "roles/storage.objectAdmin"
+
+  members = [
+    "serviceAccount:${google_service_account.thanos.email}",
+  ]
+}
+
+resource "google_service_account" "thanos" {
+  account_id = "thanos"
+  project    = var.project_id
+}
+
+resource "google_service_account_iam_binding" "thanos-pod" {
+  service_account_id = google_service_account.thanos.name
+  role               = "roles/iam.serviceAccountTokenCreator"
+
+  # thanos runs as a sidecar in the prometheus pod
+  members = ["serviceAccount:charlieegan3-cluster.svc.id.goog[monitoring/po-promop-prometheus]"]
+}
