a8ac5713da76da021c3287b54c9f868896d68f1f
Charlie Egan <charlieegan3@users.noreply.github.com>
2019-12-20 21:40:34 +0000

    Remove binding to workload account

--- gcp/charlieegan3-music/iam.tf
+++ gcp/charlieegan3-music/iam.tf
@@ -4,13 +4,6 @@ resource "google_service_account" "bigquery_uploader" {
   project      = var.project_id
 }
 
-resource "google_service_account_iam_binding" "bigquery_uploader" {
-  service_account_id = google_service_account.bigquery_uploader.name
-  role               = "roles/iam.serviceAccountTokenCreator"
-
-  members = ["serviceAccount:charlieegan3-cluster.svc.id.goog[music/music-sync]"]
-}
-
 resource "google_project_iam_binding" "upload" {
   project = var.project_id
   role    = "roles/bigquery.dataEditor"
