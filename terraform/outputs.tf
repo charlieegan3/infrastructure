1de75c3002dd4b7cc30006a7096d909f1b3861e5
Charlie Egan <charlieegan3@users.noreply.github.com>
2021-08-08 22:11:06 +0100

    Remove old projects

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,15 +1,6 @@
 output "photos_cronjob_sa_key" {
   value = google_service_account_key.photos_cronjob.private_key
 }
-output "vpn_sa_key" {
-  value = google_service_account_key.vpn.private_key
-}
 output "music_uploader_sa_key" {
   value = google_service_account_key.music_bigquery_uploader.private_key
-}
-output "vault_sa_key" {
-  value = google_service_account_key.vault.private_key
-}
-output "vault_kms_key_id" {
-  value = google_kms_crypto_key.vault.self_link
 }
