e32349492fbc76e64f262de2417a519be9ace563
Charlie Egan <charlieegan3@users.noreply.github.com>
2021-08-08 22:06:52 +0100

    Allow photo lib to be destroyed

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,6 @@
 output "photos_cronjob_sa_key" {
   value = google_service_account_key.photos_cronjob.private_key
 }
-output "photo_library_sa_key" {
-  value = google_service_account_key.photo_library.private_key
-}
 output "vpn_sa_key" {
   value = google_service_account_key.vpn.private_key
 }
--- terraform/photo-library.tf
+++ terraform/photo-library.tf
@@ -1,39 +1,9 @@
-resource "google_project" "photo_library" {
-  name       = "charlieegan3-photo-library"
-  project_id = "charlieegan3-photo-library-001"
-
-  billing_account = var.billing_account
-}
-
-resource "google_project_service" "photo_library_storage" {
-  project = google_project.photo_library.project_id
-  service = "storage-api.googleapis.com"
-}
-
-resource "google_service_account" "photo_library" {
-  project      = google_project.photo_library.project_id
-  account_id   = "runner"
-  display_name = "runner refreshing data"
-}
-
-resource "google_project_iam_binding" "photo_library_storage_admin" {
-  project = google_project.photo_library.project_id
-  role    = "roles/storage.admin"
-
-  members = [
-    "serviceAccount:${google_service_account.photo_library.email}",
-  ]
-}
-
-resource "google_service_account_key" "photo_library" {
-  service_account_id = google_service_account.photo_library.name
-}
-
 resource "google_storage_bucket" "photo_library" {
   name          = "charlieegan3-photo-library"
   location      = "EU"
-  project       = google_project.photo_library.project_id
+  project       = "charlieegan3-photo-library-001"
   storage_class = "STANDARD"
+  force_destroy = true
 
   versioning {
     enabled = false
