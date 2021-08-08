e43d0071ecf0507f957a3130ca98c8eb135b2692
Charlie Egan <charlieegan3@users.noreply.github.com>
2021-08-08 22:43:35 +0100

    Show sensitive outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,6 +1,8 @@
 output "photos_cronjob_sa_key" {
-  value = google_service_account_key.photos_cronjob.private_key
+  value     = google_service_account_key.photos_cronjob.private_key
+  sensitive = true
 }
 output "music_uploader_sa_key" {
-  value = google_service_account_key.music_bigquery_uploader.private_key
+  value     = google_service_account_key.music_bigquery_uploader.private_key
+  sensitive = true
 }
