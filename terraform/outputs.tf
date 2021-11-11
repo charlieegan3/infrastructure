1fe2da0a65ebaa15eaa027fba160f28cf393b305
Charlie Egan <charlieegan3@users.noreply.github.com>
2021-11-11 23:29:55 +0000

    Add photos cms resources

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,10 +2,17 @@ output "photos_cronjob_sa_key" {
   value     = google_service_account_key.photos_cronjob.private_key
   sensitive = true
 }
+
+output "photos_cms_sa_key" {
+  value     = google_service_account_key.photos_cms.private_key
+  sensitive = true
+}
+
 output "music_uploader_sa_key" {
   value     = google_service_account_key.music_bigquery_uploader.private_key
   sensitive = true
 }
+
 output "static_sa_key" {
   value     = google_service_account_key.static.private_key
   sensitive = true
--- terraform/photos.tf
+++ terraform/photos.tf
@@ -45,3 +45,41 @@ resource "google_storage_bucket_iam_member" "public_access" {
   role   = "roles/storage.objectViewer"
   member = "allUsers"
 }
+
+// items relating to the CMS application
+
+resource "google_storage_bucket" "cms" {
+  name          = "charlieegan3-photos-cms"
+  location      = "EU"
+  project       = google_project.photos.project_id
+  storage_class = "MULTI_REGIONAL"
+
+  versioning {
+    enabled = true
+  }
+}
+
+resource "google_storage_bucket_iam_member" "public_access_cms" {
+  bucket = google_storage_bucket.cms.name
+  role   = "roles/storage.objectViewer"
+  member = "allUsers"
+}
+
+resource "google_service_account" "photos_cms" {
+  project      = google_project.photos.project_id
+  account_id   = "photos-cms"
+  display_name = "CMS"
+}
+
+resource "google_project_iam_binding" "storage_admin_cms" {
+  project = google_project.photos.project_id
+  role    = "roles/storage.admin"
+
+  members = [
+    "serviceAccount:${google_service_account.photos_cms.email}",
+  ]
+}
+
+resource "google_service_account_key" "photos_cms" {
+  service_account_id = google_service_account.photos_cms.name
+}
